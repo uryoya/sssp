21edc13668f58ce04a7affc8c826e36d828a14c8
URANO Ryoya <urano.works.mail@gmail.com>
2018-03-14 16:07:53 +0900

    DSP側がタイムアウトしても処理が落ちないようにした

--- src/main/scala/com/uryoya/sssp/controller/AdController.scala
+++ src/main/scala/com/uryoya/sssp/controller/AdController.scala
@@ -2,9 +2,10 @@ package com.uryoya.sssp.controller
 
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http
-import com.twitter.util.{Await, Future, Duration}
+import com.twitter.util.Future
 import com.twitter.conversions.time._
-import io.circe.Encoder
+import com.twitter.finagle.service.TimeoutFilter
+import com.twitter.finagle.util.DefaultTimer
 import io.circe.syntax._
 import io.circe.generic.auto._
 import io.circe.parser._
@@ -13,20 +14,13 @@ import com.uryoya.sssp.config
 
 object AdController {
   def exhibit(req: AdExhibitRequest): Future[Either[Exception, AdExhibitResponse]] = {
-    val requests: List[Future[http.Response]] = config.sssp.buyers.map { buyer =>
-      val url = new java.net.URI(buyer)
-      val client: Service[http.Request, http.Response] = Http.client
-        .withRequestTimeout(config.sssp.timeout.millisecond)
-        .newService(s"${url.getHost}:${url.getPort}")
-      val request = http.Request(http.Method.Post, url.getPath)
-      request.host = url.getHost
-      request.setContentTypeJson
-      request.setContentString(AdBidRequest(100).asJson.noSpaces)
-      client(request)
-    }
-    Await.ready(Future.collect(requests), config.sssp.timeout.milliseconds).map { responses =>
+    val requests: Future[Seq[Either[Throwable, http.Response]]] = Future.collect(
+      config.sssp.buyers.map(buyer => requestToDsp(new java.net.URI(buyer)))
+    )
+    requests.map { responses =>
       val bids = for {
-        response <- responses
+        maybeResponse <- responses
+        response <- maybeResponse.toOption
         if response.status == http.Status.Ok
         bid <- decode[AdBidResponse](response.getContentString).toOption
       } yield bid
@@ -35,5 +29,27 @@ object AdController {
       else
         Right(AdExhibitResponse(bids.max.url))
     }
+  }
+
+  private def requestToDsp(url: java.net.URI): Future[Either[Throwable, http.Response]] = {
+    // 1. Service[ReqOut, RepIn] のタイムアウトを設定する。
+    val timeoutFilter =
+      new TimeoutFilter[http.Request, http.Response](config.sssp.timeout.seconds, DefaultTimer.getInstance)
+    // 2. クライアント(Service[ReqOut, ReqIn])を作成する。このときTimeoutFilterを加える。
+    val client: Service[http.Request, http.Response] = timeoutFilter andThen  Http.client
+      // 3. このタイムアウトはHTTPのタイムアウト設定。
+      .withRequestTimeout(config.sssp.timeout.millisecond)
+      .newService(s"${url.getHost}:${url.getPort}")
+
+    // 4. リクエストボディを構築する。
+    val request = http.Request(http.Method.Post, url.getPath)
+    request.host = url.getHost
+    request.setContentTypeJson
+    request.setContentString(AdBidRequest(100).asJson.noSpaces)
+
+    // 5. 例外が送出されるとFuture全体が落ちるので、Eitherで包む。
+    client(request)
+      .map(Right(_))
+      .handle { case t => Left(t) }
   }
 }
