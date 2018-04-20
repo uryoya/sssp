281474e5bffa01a3bd2df03e3faf4fd0bb2042f1
URANO Ryoya <urano.works.mail@gmail.com>
2018-04-20 14:34:43 +0900

    スネークケースを使うJSONに対してデコーダ・エンコーダを定義

--- src/main/scala/com/uryoya/sssp/entity/AdBidRequest.scala
+++ src/main/scala/com/uryoya/sssp/entity/AdBidRequest.scala
@@ -1,5 +1,14 @@
 package com.uryoya.sssp.entity
 
+import io.circe.{Decoder, Encoder}
+
 final case class AdBidRequest (
   appId: Int,
 )
+
+object AdBidRequest {
+  implicit val decodeAdBidRequest: Decoder[AdBidRequest] =
+    Decoder.forProduct1("app_id")(AdBidRequest.apply)
+  implicit val encodeAdBidRequest: Encoder[AdBidRequest] =
+    Encoder.forProduct1("app_id")(a => a.appId)
+}
--- src/main/scala/com/uryoya/sssp/entity/AdExhibitRequest.scala
+++ src/main/scala/com/uryoya/sssp/entity/AdExhibitRequest.scala
@@ -1,5 +1,14 @@
 package com.uryoya.sssp.entity
 
+import io.circe.{Decoder, Encoder}
+
 final case class AdExhibitRequest (
   appId: Int,
 )
+
+object AdExhibitRequest {
+  implicit val decodeAdBidRequest: Decoder[AdExhibitRequest] =
+    Decoder.forProduct1("app_id")(AdExhibitRequest.apply)
+  implicit val encodeAdBidRequest: Encoder[AdExhibitRequest] =
+    Encoder.forProduct1("app_id")(a => a.appId)
+}
